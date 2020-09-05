d1a94d91e9988192493474a40355c2e9bb3aad50
Mireia Farre <67146405+farremireia@users.noreply.github.com>
2020-09-05 15:49:14 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,46 @@
+<?php
+$play_count = 0;
+$correct_guesses = 0;
+$guess_high = 0;
+$guess_low = 0;
+
+echo "I'm going to think of numbers between 1 and 10 (inclusive). Do you think you can guess correctly?\n";
+
+function guessNumber() {
+  global $play_count, $correct_guesses, $guess_high, $guess_low;
+
+  $play_count++;
+
+  $num = rand(1, 10);
+
+  echo "\nMake your guess..\n";
+
+  $guess = readline(">> ");
+  $guess = intval($guess);
+
+  echo "Round: $play_count\nMy number: $num\nYour guess: $guess";
+
+  if ($guess === $num){
+    $correct_guesses++;
+  }
+
+  if ($guess > $num){
+    $guess_high++;
+  }
+
+  if ($guess < $num){
+    $guess_low++;
+  }
+}
+
+guessNumber();
+
+$percent_correct = $correct_guesses / $play_count * 100;
+
+echo "\nAfter $play_count round, here are some facts about your guessing:\nYou guessed the number correctly $percent_correct% of the time.\n";
+
+if ($guess_high > $guess_low) {
+  echo "When you guessed wrong, you tended to guess high.";
+} elseif ($guess_high < $guess_low) {
+  echo "When you guessed wrong, you tended to guess low.";
+}
